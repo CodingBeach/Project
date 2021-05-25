5c8da8759eb1730d23ad253cf030ed507c2c369a
CodingBeach <codingbeach@outlook.com>
2021-05-25 13:18:33 +0200

    Zmienilem

--- Launcher.py
+++ Launcher.py
@@ -23,6 +23,7 @@ input_password_rect = pygame.Rect(390,254,500,48)
 button = pygame.image.load('assets/playbutton.png')
 buttonClicked = pygame.image.load('assets/playbutton_clicked.png')
 #xxxxxxxxxxxxxxxxxx
+#jerjoaeojraeojr
 
 ## Video Represent
 def exitOp(isAudio,videoFlag,audiothread): ## just exiting the threads needed for the video displaying. CALL THIS WHEN EXITING THE CLIP
